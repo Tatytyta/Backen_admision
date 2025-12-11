2a5eb51b3ceb58e5dcc45cd60799c2d39f988e53
Tatytyta <sandra14taty@gmail.com>
2025-12-10 19:57:35 -0500

    creacion del archivo para deploy

--- admision_api/settings.py
+++ admision_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-s=wq5^-=&-nauyx4wn6q1vu4$76hu0_tdq79d6zm_)y5+c)r9h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -147,6 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -157,4 +158,3 @@ SIMPLE_JWT = {
   'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
   'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
-
